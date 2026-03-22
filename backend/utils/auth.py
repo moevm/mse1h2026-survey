01b8e0397ea4bdeb1f92ec3739566525bbd0785a
Лодыгин Иван <144445186+IvanLodygin@users.noreply.github.com>
2026-03-22 17:35:21 +0300

    added auth

---
+++ backend/utils/auth.py
@@ -0,0 +1,68 @@
+import os
+from datetime import datetime, timedelta, timezone
+from typing import Optional
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from fastapi import Depends, HTTPException, Request
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+import models, database
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv('JWT_SECRET_KEY', default="SUPER_SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+
+class OAuth2PasswordBearerWithCookie(OAuth2PasswordBearer):
+    async def __call__(self, request: Request) -> Optional[str]:
+        token: str = request.cookies.get("access_token")
+        
+        if token and token.startswith("Bearer "):
+            return token.split(" ")[1]
+        
+        return None
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearerWithCookie(tokenUrl="login", auto_error=False)
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def get_current_user_optional(
+    token: Optional[str] = Depends(oauth2_scheme), 
+    db: Session = Depends(database.get_db)
+) -> Optional[models.User]:
+    if not token:
+        return None
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            return None
+    except JWTError:
+        return None
+    
+    return db.query(models.User).filter(models.User.username == username).first()
+
+
+def get_current_user_required(current_user: models.User = Depends(get_current_user_optional)):
+    if not current_user:
+        raise HTTPException(status_code=401, detail="Необходима авторизация")
+    return current_user

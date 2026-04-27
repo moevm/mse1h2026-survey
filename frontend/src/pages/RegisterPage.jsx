b5623e7b9eacfd10fc048aba061fbbff72b3da27
Devilpoper <mengee101@gmail.com>
2026-04-27 19:56:39 +0300

    add login and register page

---
+++ frontend/src/pages/RegisterPage.jsx
@@ -0,0 +1,27 @@
+import { SpotLayout } from '@shared/ui/layouts/spot-layout';
+import { Header } from '@widgets/header';
+import { Main } from "@widgets/main";
+import { Footer } from '@widgets/footer';
+import { Container } from '@shared/ui/container';
+import { UserAuthForm } from '@widgets/user-auth-form';
+import LogoIcon from "@shared/assets/icons/logo.svg?react";
+
+export const RegisterPage = () => {
+  return (
+    <>
+      <Header>
+        <Container>
+          <LogoIcon />
+        </Container>
+      </Header>
+      <Main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <SpotLayout>
+          <Container>
+            <UserAuthForm mode="register" />
+          </Container>
+        </SpotLayout>
+      </Main>
+      <Footer />
+    </>
+  );
+};
